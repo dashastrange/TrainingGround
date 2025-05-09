d36b220b987e0387b00718582927da75f8039318
Daria Vasylieva <dariavasylieva@Darias-MacBook-Air.local>
2025-05-09 13:19:27 +0200

    Working with my own small data

--- BasicDataExploration.py
+++ BasicDataExploration.py
@@ -1,2 +1,8 @@
 import pandas as pd
+import numpy as np
 
+cases_file_path = '/Users/dariavasylieva/PycharmProjects/TrainingGround/TestCasePriority.csv'
+cases_data = pd.read_csv(cases_file_path)
+#print(cases_data.describe())
+#print(cases_data.columns)
+print(cases_data.PriorityFromPO)
