6cb6d6e95c186594f086cf8d68f5cb9910bd5beb
Daria Vasylieva <dariavasylieva@Darias-MacBook-Air.local>
2025-05-09 13:45:00 +0200

    Beautify all

--- BasicDataExploration.py
+++ BasicDataExploration.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import numpy as np
 
-cases_file_path = '/Users/dariavasylieva/PycharmProjects/TrainingGround/TestCasePriority.csv'
-cases_data = pd.read_csv(cases_file_path)
-#print(cases_data.describe())
-#print(cases_data.columns)
-print(cases_data.PriorityFromPO)
+#save path to variable
+houses_file_path = '/Users/dariavasylieva/PycharmProjects/TrainingGround/melbourneHousing.csv'
+#read data and store them in DataFrame
+melbourne_data = pd.read_csv(houses_file_path)
+#print summary of the data
+print(melbourne_data.describe())
+print('--------------------------------------------------------------------------------------')
+#select data for modelling
+print(melbourne_data.columns)
+print('--------------------------------------------------------------------------------------')
+#select Prediction Target
+y = melbourne_data.Price
+print(y)
+print('--------------------------------------------------------------------------------------')
+#select Features
+melbourne_features = ["Rooms", "Bathroom", "Landsize", "Lattitude", "Longtitude"]
+x = melbourne_data[melbourne_features]
+print(x)
+print('--------------------------------------------------------------------------------------')
+
